abdeb914949622640c20146fcc9104ed4387b82c
jab0122 <jzb0172@auburn.edu>
2021-11-29 23:57:16 -0600

    added beta estimation, R2 and Cp

--- R/AyaJuliapackage.R
+++ R/AyaJuliapackage.R
@@ -9,4 +9,66 @@
 #' @importFrom stats runif
 #' @export
 #' @examples
-#' 
+#'
+#'
+
+set.seed(123)
+
+#I created random values for x and y to test the functions
+x <- matrix(rnorm(400), ncol = 4)
+y <- rnorm(100)
+alpha <- 0.05
+
+my_lm <- function(y, x, alpha = 0.05) {
+
+  # Define parameters
+  n <- length(y)
+  X <- cbind(rep(1, n), x)
+  y_mean <- mean(y)
+
+  #Set initial beta
+  beta_zero <- c(y_mean)
+
+  for (i in 2:ncol(X)){
+    beta_zero[i] <- cov(y, X[,i])/var(X[,i])
+  }
+
+  #Define loss function
+  loss_func <- function(y, X, par){
+    H <- t(y - X%*%par) %*% (y - X%*%par)
+    return(H)
+  }
+
+  # Estimate beta through loss function
+  beta.hat <- optim(par = beta_zero, fn = loss_func, X = X, y = y, method = "BFGS")
+
+  #Calculating R squared
+  y_hat <- X %*% as.matrix(beta.hat$par)
+  sse <- sum((y - y_hat)^2)
+  sst <- sum((y - y_mean)^2)
+
+  R_squared <- 1-(sse/sst)
+
+  #Calculating Cp
+  resid <- y - X%*%as.matrix(beta.hat$par)
+  p <- dim(X)[2]
+  df <- n - p
+  sigma2.hat <- (1/df)*t(resid)%*%resid
+
+  Cp <- sse - 2*p*sigma2.hat
+
+  #Calculating confidence intervals
+  quant <- 1 - alpha/2
+  ci.beta
+
+
+  # Return all estimated values
+  return(list(beta = c(beta.hat$par), sigma2 = sigma2.hat, Cp = Cp, R2 = R_squared, ci = ci.beta))
+
+
+
+}
+
+plot(y ~ X[,2], data = , main="Least square error regression for x")
+abline(a = beta.hat$par[1], b = beta.hat$par[2], col = "red")
+
